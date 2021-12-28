a91f83387201c8b35c7dccbe14067bf410b5c002
Iva <iva@local>
2021-12-28 06:48:48 +0100

    Show info when hovering over the graph, automatically show graph at the end of the lesson

--- Assets/DrawGraph.cs
+++ Assets/DrawGraph.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class DrawGraph:Graphic{
-	public Color32 selectionColor;
+	public Color32 diamondColor;
 	public float selectionSize=32;
 	public float lineWidth=5;
 	public bool fillArea;
@@ -10,19 +10,20 @@ public class DrawGraph:Graphic{
 	public float[] values;
 	public float timeScale;
 	public float[] times;
-	[HideInInspector]public int currentIndex;
-	[HideInInspector]public int hoverIndex=-1;
-	[HideInInspector]public float expandedBlend;
+	[System.NonSerialized]public int currentIndex;
+	[System.NonSerialized]public int hoverIndex=-1;
+	[System.NonSerialized]public float expandedBlend;
 	float width,height;
 
 	protected override void OnPopulateMesh(VertexHelper vh){
+		vh.Clear();
+		
 		Rect rect=rectTransform.rect;
 		width=rect.width;
 		height=rect.height;
-		
-		vh.Clear();
-		
+
 		UIVertex vertex=UIVertex.simpleVert;
+		vertex.color=color;
 		if(currentIndex<2||values.Length<=1)
 			return;
 		
@@ -34,7 +35,7 @@ public class DrawGraph:Graphic{
 			float currentPosX=width*(times[i]-times[0])/(timeScale-times[0]);
 			if(expandedBlend>0.00001f){
 				float diff=Mathf.Abs(mouseHoverPos-currentPosX);
-				if(diff<lowestHoverDiff){
+				if(diff<=lowestHoverDiff){
 					hoverPointX=currentPosX;
 					lowestHoverDiff=diff;
 					hoverIndex=i;	//TODO: Only select a point if the mouse is hovering over the graph (within the RectTransform boundaries)
@@ -42,7 +43,6 @@ public class DrawGraph:Graphic{
 			}
 			if(i==0) continue;
 			if(times[i]<times[i-1]) return;
-			vertex.color=color; //TODO: Maybe color the vertices red where the user makes an error? (right side of current & left side of next)
 			
 			float previousPosX=width*(times[i-1]-times[0])/(timeScale-times[0]);
 			
@@ -85,7 +85,7 @@ public class DrawGraph:Graphic{
 		if(hoverIndex>-1){
 			Color32 inactiveColor=vertex.color;
 			inactiveColor.a=0;
-			vertex.color=Color32.Lerp(inactiveColor,selectionColor,expandedBlend);
+			vertex.color=Color32.Lerp(inactiveColor,diamondColor,expandedBlend);
 			
 			float hoverPointY=height*values[hoverIndex]/valueScale;
 			
@@ -105,13 +105,8 @@ public class DrawGraph:Graphic{
 			vh.AddTriangle(vertCount-1,vertCount-2,vertCount-3);
 			vh.AddTriangle(vertCount-3,vertCount-4,vertCount-1);
 		}
-
 		
-		//TODO: Draw a tooltip at the cursor's position when hovering over the graph points (possibly in another component?) Also draw a circle (or diamond) over the currently selected point
-		
-		// Maybe instead of a tooltip, show text where the quote would ordinarily be?
-		
-		//TODO: Draw error rate curve
+		//TODO: Draw error rate curve (or do the MonkeyType thing and draw x-es where errors occur (or just a triangle if you are lazy))
 		
 		//TODO: Draw another curve for the "full-word" speed, but draw it "sharply" (as in, no diagonal lines, just horizontal and vertical)
 		/*
