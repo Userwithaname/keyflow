ecd677ca3d5a74595baca82259e3c169ae3d6a00
Userwithaname <42674728+Userwithaname@users.noreply.github.com>
2025-08-11 10:58:25 +0200

    Improved code readability and small improvement - Improved code formatting - DrawGraph.cs: Support for other scaling modes

--- Assets/SliderColors.cs
+++ Assets/SliderColors.cs
@@ -6,21 +6,21 @@ using UnityEngine.UI;
 
 //TODO: Idea: Add a colored underline to give the user a useful representation of what the current key practice confidence is compared to the rest (or what each of the values represent compared to the average)
 
-public class SliderColors:MonoBehaviour{
+public class SliderColors : MonoBehaviour {
     Slider slider;
     Image image;
 
-    void Start(){
-        slider=GetComponent<Slider>();
-        image=slider.fillRect.GetComponent<Image>();
+    void Start() {
+        slider = GetComponent<Slider>();
+        image = slider.fillRect.GetComponent<Image>();
     }
-    void Update(){
-        switch(slider.value){
+    void Update() {
+        switch(slider.value) {
             case <.5f:
-                image.color=Color.Lerp(Color.red,Color.yellow,slider.value*2);
+                image.color = Color.Lerp(Color.red, Color.yellow, slider.value * 2);
                 break;
             default:
-                image.color=Color.Lerp(Color.yellow,Color.green,(slider.value-.5f)*2);
+                image.color = Color.Lerp(Color.yellow, Color.green, (slider.value - .5f) * 2);
                 break;
         }
     }
